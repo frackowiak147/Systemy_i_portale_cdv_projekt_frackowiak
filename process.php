2beda746063647a7f4943d5721149ed710498ca2
Jan <72294368+frackowiak147@users.noreply.github.com>
2024-07-02 19:12:57 +0200

    Update process.php

--- process.php
+++ process.php
@@ -1,5 +1,37 @@
 <?php
 echo "cos";
+
+try {
+    $dsn = "sqlsrv:server = tcp:developerlife2.database.windows.net,1433; Database = kolko-krzyzyk";
+    $username = "jfrackowiak"; // Spróbuj użyć tylko nazwy użytkownika, bez domeny
+    $password = "qM@83Ha8WkB";
+
+    // Tworzenie nowego połączenia PDO
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Zapytanie SQL
+    $sql = "SELECT TOP 2 * FROM gracze ORDER BY id DESC";
+    $stmt = $conn->query($sql);
+    
+    // Fetchowanie danych
+    $gracze = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($gracze) > 0) {
+        $response = array(
+            'gracz1' => isset($gracze[0]['nazwa']) ? $gracze[0]['nazwa'] : 'Brak nazwy',
+            'gracz2' => isset($gracze[1]['nazwa']) ? $gracze[1]['nazwa'] : 'Brak nazwy'
+        );
+        echo json_encode($response);
+    } else {
+        echo json_encode(array('error' => 'Brak danych'));
+    }
+} catch (PDOException $e) {
+    echo "Błąd połączenia: " . $e->getMessage();
+}
+?>
+
+/*
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:developerlife2.database.windows.net,1433; Database = kolko-krzyzyk", "jfrackowiak@edu.cdv.pl", "qM@83Ha8WkB");
@@ -16,7 +48,7 @@ $serverName = "tcp:developerlife2.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
 
-/*
+////////////////////////////////////////////////////////////
 $servername = "tcp:developerlife2.database.windows.net,1433";
 $username = "jfrackowiak@edu.cdv.pl";
 $password = "qM@83Ha8WkB";
