4e411a1c02478e02753a1b71161acc06cd54dfb1
Jan <72294368+frackowiak147@users.noreply.github.com>
2024-07-04 10:08:08 +0200

    Update gra.php

--- gra.php
+++ gra.php
@@ -1,3 +1,30 @@
+<?php
+// Połączenie z bazą danych
+try {
+    $dsn = "sqlsrv:server = tcp:developerlife2.database.windows.net,1433; Database = kolko-krzyzyk";
+    $username = "jfrackowiak@edu.cdv.pl@developerlife2";
+    $password = "qM@83Ha8WkB";
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Pobieranie nazw graczy
+    $stmt = $conn->query("SELECT nazwa FROM gracze");
+    $gracze = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Sprawdzanie czy rekordy istnieją
+    if (count($gracze) >= 2) {
+        $gracz1Name = $gracze[0]['nazwa'];
+        $gracz2Name = $gracze[1]['nazwa'];
+    } else {
+        $gracz1Name = "Gracz 1"; // Domyślne wartości w przypadku braku danych
+        $gracz2Name = "Gracz 2";
+    }
+} catch (PDOException $e) {
+    echo "Błąd połączenia: " . $e->getMessage();
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -14,8 +41,8 @@
             <button class="exit-button" onclick="powrotDoIndex()">X</button>
         </div>
         <div class="content">
-            <h2 id="gracz1" class="nazwa-gracza"><?php echo "$gracz1Name"; ?>Gracz 1</h2>
-            <h2 id="gracz2" class="nazwa-gracza"><?php echo "$gracz2Name"; ?>Gracz 2</h2>
+            <h2 id="gracz1" class="nazwa-gracza"><?php echo htmlspecialchars($gracz1Name); ?></h2>
+            <h2 id="gracz2" class="nazwa-gracza"><?php echo htmlspecialchars($gracz2Name); ?></h2>
             <div class="plansza-krzyzyk-container">
                 <div class="plansza-krzyzyk">
                     <div id="pole1" class="pole" onclick="zaznaczPole(1)"></div>
